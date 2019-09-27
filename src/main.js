529bc5856327de323a4f04fa5940d43c0228ef95
Yash Rathore <yash@vitwit.com>
2019-09-27 18:36:25 +0530

    export utils

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { Schedular } from './scheduleTasks';
 import { CodeGen } from './codgen';
 import { CodePlusDocGen } from './docgen';
 import { parseArgumentsIntoOptions } from './cli'
+import * as utils from './utils'
 
 export {
   Schedular,
   CodeGen,
   CodePlusDocGen,
-  parseArgumentsIntoOptions
+  parseArgumentsIntoOptions,
+  utils
 }
